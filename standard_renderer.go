80b9ad29e9b1fb2d71b94369b96c746a4aed1738
Braedon Wooding <braedonww@gmail.com>
2025-08-28 13:03:33 +1000

    Bring over patch

--- standard_renderer.go
+++ standard_renderer.go
@@ -210,7 +210,10 @@ func (r *standardRenderer) flush() error {
 		buf.WriteString(ansi.CursorBackward(r.width))
 	}
 
+	// @BW: Changed to enable synchronized output.
+	_, _ = r.out.Write([]byte(ansi.SetSynchronizedOutputMode))
 	_, _ = r.out.Write(buf.Bytes())
+	_, _ = r.out.Write([]byte(ansi.ResetSynchronizedOutputMode))
 	r.lastRender = r.buf.String()
 
 	// Save previously rendered lines for comparison in the next render. If we
